2e8dfe24914668cd4ad6176148a364f0630fe121
Vrentadax <vrentadax@outlook.com>
2022-06-28 19:21:28 -0600

    added query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,10 +3,35 @@ const { User, Opinion } = require('../models');
 
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('opinions');
+
+                return userData;
+            }
+
+            throw new AuthenticationError('You must be logged in.');
+        },
+        users: async () => {
+            return User.find()
+                .select('-__v -password')
+                .populate('opinions');
+        },
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+                .select('-__v -password')
+                .populate('opinions');
+        },
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 });
+        },
+        thought: async (parent, { id }) => {
+            return Thought.findOne({ _id });
         }
-    }
+    },
 };
 
 
